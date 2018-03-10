c78167998e5ee0f84136af525ddb797521a618aa
rnsuarel <rodrigo.suarez.lausz@gmail.com>
2018-03-10 19:38:32 -0300

    hh

--- base/src/main/java/com/proycc/base/controller/AuditoriaController.java
+++ base/src/main/java/com/proycc/base/controller/AuditoriaController.java
@@ -2,8 +2,10 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * MIKUS
  */
 package com.proycc.base.controller;
+
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
